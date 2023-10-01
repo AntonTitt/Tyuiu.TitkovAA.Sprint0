3e145de8609a00a7224d774ee2bb123caf09ee0a
Антон <vip.tank2005@outlook.com>
2023-10-01 13:57:42 +0500

    Task2

---
+++ Tyuiu.TitkovAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.TitkovAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.TitkovAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessegeValid()
+        {
+            var name = "Антор";
+            var res = DataService.GetMessege(name);
+
+            Assert.AreEqual("Привет, Антор", res);
+        }
+    }
+}
---
+++ Tyuiu.TitkovAA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TitkovAA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TitkovAA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d91ec51b-572f-4adb-a62e-6905009b0a21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
